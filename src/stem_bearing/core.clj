ab6c2eedb3f879d94acdf2bea41a90a4e0b6ad77
OJIMA Ryoji <rojima1@gmail.com>
2015-08-06 10:00:56 +0900

    Some macros are added.

--- src/stem_bearing/core.clj
+++ src/stem_bearing/core.clj
@@ -1,13 +1,18 @@
 (ns stem-bearing.core
   (:require (clojure [pprint :as pprint])))
 
-(defn pformat
-  "Format an object with pprint formatter."
-  [object]
-  (->> (with-out-str
-         (pprint/pprint object))
-       (butlast)
-       (apply str)))
+(defmacro pprint-macro
+  [form]
+  `(pprint/pprint (macroexpand-1 '~form)))
+
+(defmacro with-map-bindings
+  [binding-forms function & body]
+  `(let [[~@binding-forms] (map ~function [~@binding-forms])]
+     ~@body))
+
+(defn flip
+  [function]
+  (fn [arg & more] (apply function (concat more [arg]))))
 
 (defn dissoc-in
   "Dissociates a value in a nested associative structure, and returns a new map that does not contain a mapping for keys."
@@ -15,3 +20,11 @@
   (if more
     (assoc  map key (dissoc-in (get map key) more))
     (dissoc map key)))
+
+(defn pformat
+  "Format an object with pprint formatter."
+  [object]
+  (->> (with-out-str
+         (pprint/pprint object))
+       (butlast)
+       (apply str)))
